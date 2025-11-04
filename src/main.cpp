0d64f9f9d01abd6efd1979b7fc930d6192c7ccbe
schoi88 <seanchoi2239@gmail.com>
2025-11-03 23:47:02 -0500

    Vec3 and Color

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@ and will include some practices from my time in Operating Systems to
 hopefully improve the performance of the program. :)
 */
 
+#include "Color.h"
+#include "Vec3.h"
+
 #include <iostream>
 
 int main(){
@@ -17,15 +20,9 @@ int main(){
 
     for(int h = 0; h < image_height; h++){
         for(int w = 0; w < image_width; w++){
-            double r = double(w) / (image_width - 1);
-            double g = double(h) / (image_height - 1);
-            double b = 0.0;
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            Color pixel_color = Color(double(w) / (image_width - 1), 
+                double(h) / (image_height - 1), 0);
+            write_color(std::cout, pixel_color);
         }
     }
 
