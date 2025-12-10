294e55b863bbbcbf45282ad1ba3ed46899db9984
schoi88 <seanchoi2239@gmail.com>
2025-12-09 22:21:01 -0500

    added surface normals and HittableList class

--- src/Hittable.h
+++ src/Hittable.h
@@ -16,6 +16,33 @@ class HitRecord{
         Vec3 normal;
         //the root value of hit (result of quadratic formula)
         double t;
+        /*
+        front_face will determine which side (inside or outside) of the surface
+        of an object the ray is originating from, which can be important for how
+        light will affect objects with different materials like metal or glass.
+        */
+        bool front_face;
+
+        /*
+        This function will set the side of the surface a ray is coming from.
+        It can be determined by using a dot product of the ray's direction and
+        the outward_normal of the object.
+
+        ***outward_normal will need to be of unit length, this is to be done in
+           object classes as it will be dependent on the object's geometric shape.
+
+        input:
+            r: the ray
+            outward_normal: the outside surface of the object
+
+        output:
+            True if ray is outside the object (dot product is < 0) or false if
+            ray is within the surface of object (dot product is > 0).
+        */
+        void set_face_normal(const Ray& r, const Vec3& outward_normal){
+            front_face = dot(r.direction(), outward_normal) < 0;
+            normal = front_face ? outward_normal : -outward_normal;
+        }
 };
 
 class Hittable{
